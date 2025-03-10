e38b9ab4893b86b447edbc64ff66652744da1fbb
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2025-03-10 18:27:43 +0100

    sync

---
+++ config.el
@@ -0,0 +1,284 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Antonio Petrillo"
+      user-mail-address "antonio.petrillo4@studenti.unina.it")
+
+(setq doom-theme 'doom-one)
+
+(setq display-line-numbers-type 'relative)
+(setq org-hide-emphasis-markers t)
+
+(setq org-directory "~/Documents/Org/")
+(setq denote-directory (expand-file-name "notes" "~/Documents/Org"))
+(setq org-agenda-files `(,(expand-file-name "Agenda.org" org-directory)
+                         ,(expand-file-name "Uni.org" org-directory)))
+
+(setq org-agenda-custom-commands
+      `(
+        ("d" "Daily Agenda"
+         ((agenda ""
+                  ((org-agenda-span 'day)
+                   (org-deadline-warning-days 7)
+                   (org-agenda-format-date "%A %-e %B %Y")
+                   (org-agenda-overriding-header "Today 📆")))
+          (todo "WAIT"
+                ((org-agenda-overriding-header "Waiting tasks 🕙")))))
+
+        ("r" "Reading List"
+         ((tags "reading"
+                ((org-agenda-overriding-header "Currently reading 📖")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'regexp "^\\* Reading list .*"))))
+          (tags "+book-reading"
+                ((org-agenda-overriding-header "Next to read 📚")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'regexp "^\\* Reading list .*"))))))
+
+        ("g" "Getting Things Done"
+         ((tags "inbox"
+                ((org-agenda-overriding-header "Inbox: 📬")))
+          (alltodo "TODO"
+                   ((org-agenda-overriding-header "Act: 📌")))
+          (tags "explore" ;; refine
+                ((org-agenda-overriding-header "Explore: 🔭")))))
+
+        ("p" "Projects"
+         ((tags "proj"
+                ((org-agenda-overriding-header "Projects: 🛠️")
+                 (org-agenda-skip-if 'done)))
+          (tags "proj"
+                ((org-agenda-overriding-header "Completed: ⚒️✅️")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo 'done))))))
+
+        ("u" "Uni"
+         ((tags "+uni-exam"
+                ((org-agenda-overriding-header "Uni: 🎓")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'regexp "^\\* Uni .*"))))
+          (tags "exam"
+                ((org-agenda-overriding-header "Exams todo: 📄")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo '("PROGRESS")))))
+          (tags "exam"
+                ((org-agenda-overriding-header "Current courses: 🏢")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo '("TODO")))))
+          (tags "exam"
+                ((org-agenda-overriding-header "Remaining exams: 📆")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo '("WAIT")))))
+          (tags "+uni+proj"
+                ((org-agenda-overriding-header "Projects: 💻")))
+          (tags "exam done"
+                ((org-agenda-overriding-header "Exams Completed: 📄✅")
+                 (org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo 'done))))))))
+
+(use-package! ace-window
+  :custom
+  (aw-scope 'frame)
+  (aw-dispatch-always nil)
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-dispatch-alist
+   '((?x aw-delete-window "Delete Windows")
+     (?m aw-swap-window "Swap Windows")
+     (?M aw-move-window "Move Windows")
+     (?c aw-move-window "Move Windows")
+     (?j aw-switch-buffer-in-window "Select Buffer")
+     (?f aw-flip-window "Previous window")
+     (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+     (?e aw-execute-command-other-window "Execute Command Other Window")
+     (?S aw-split-window-fair "Split Fair Window")
+     (?v aw-split-window-vert "Split Vert Window")
+     (?b aw-split-window-horz "Split Horz Window")
+     (?o delete-other-windows "Delete Other Windows")
+     (?? aw-show-dispatch-help "Help"))))
+
+(use-package! dired-hide-dotfiles
+  :custom (dired-listing-switches "-agho --group-directories-first")
+  (add-hook! 'dired-mode-hook #'dired-hide-dotfiles-mode)
+  (map! :map dired-mode-map
+        "C-h" #'dired-hide-dotfiles-mode
+        "h" #'dired-up-directory
+        "l" #'dired-find-file
+        "m" #'dired-mark
+        "t" #'dired-toggle-marks
+        "u" #'dired-unmark
+        "C" #'dired-do-copy
+        "D" #'dired-do-delete
+        "J" #'dired-goto-file
+        "M" #'dired-do-chmod
+        "O" #'dired-do-chown
+        "R" #'dired-do-rename
+        "T" #'dired-do-touch
+        "Y" #'dired-copy-filename-as-kill
+        "+" #'dired-create-directory
+        "-" #'dired-up-directory
+        "% l" #'dired-downcase
+        "% u" #'dired-upcase
+        "; d" #'epa-dired-do-decrypt
+        "; e" #'epa-dired-do-encrypt)
+  (setq delete-by-moving-to-trash t))
+
+(after! which-key
+
+  (setq which-key-idle-delay 0.5)
+  (setq which-key-allow-multiple-replacements t)
+
+  (pushnew!
+   which-key-replacement-alist
+   '(("" . "\\`+?evil[-:]?\\(?:a-\\)?\\(.*\\)") . (nil . "◂\\1"))
+   '(("\\`g s" . "\\`evilem--?motion-\\(.*\\)") . (nil . "◃\\1"))))
+
+(use-package! google-translate
+  :config
+  (set-popup-rule! "^\\*Google Translate" :slot -1 :size 0.2 :select t)
+  (setq google-translate-translation-directions-alist
+        '(("it" . "en") ("en" . "it")))
+  (setq google-translate-default-source-language "it")
+  (setq google-translate-default-target-language "en"))
+
+;; kill word hack
+(defun nto/backward-kill-word()
+  "Same as `backward-kill-word' but if it is invoked on a white space character
+at the beginning of the line it will stop at it, furthermore if it is invoked
+on the beginning of the line it will go the end of the previous line instead
+of delete the previous word."
+  (interactive)
+  (let ((same? (save-excursion
+                 (let ((orig (line-number-at-pos (point)))
+                       (dest (progn
+                               (backward-word)
+                               (line-number-at-pos (point)))))
+                   (eq orig dest))))
+        (start? (eq (point) (line-beginning-position))))
+    (cond (start? (backward-delete-char 1))
+          (same? (backward-kill-word 1))
+          (:else (kill-line 0)))))
+
+(map!
+ (:gni
+  :desc "next line" "C-n" #'next-line
+  :desc "prev line" "C-p" #'previous-line
+  :desc "delete char" "C-d" #'delete-char
+  :desc "kill word" "M-d" #'kill-word)
+ (:g
+  :desc "Consult yank" "M-y" #'consult-yank-pop
+  :desc "Delete backward" "C-<backspace>" #'nto/backward-kill-word)
+ (:gnvi
+  :desc "next-tab" "C-<tab>" #'tab-next
+  :desc "previous-tab" "S-C-<tab>" #'tab-previous)
+ (:leader
+  :desc "Exec Cmd" :n "SPC" #'execute-extended-command
+  :desc "Exec Cmd for buffer" :n "M-SPC" #'execute-extended-command-for-buffer
+  (:prefix ("f" . "file")
+   :desc "Grep file" "g" #'consult-ripgrep
+   :desc "Find file" "f" #'consult-find)
+  (:prefix ("j" . "jump")
+   :desc "jump to char" "j" #'avy-goto-char-timer
+   :desc "consult line" "c" #'consult-line
+   :desc "jump to char 2" "J" #'avy-goto-char-2
+   :desc "jump to word" "w" #'avy-goto-word-0
+   :desc "jump to line" "l" #'avy-goto-line
+   :desc "jump to line below" "n" #'avy-goto-line-below
+   :desc "jump to line above" "p" #'avy-goto-line-above
+   :desc "jump to end line" "e" #'avy-goto-end-of-line)
+
+  (:prefix ("l". "lang")
+   :desc "translate (it -> en)" "p" #'google-translate-at-point
+   :desc "translate (en -> it)" "P" #'google-translate-at-point-reverse)
+
+  (:prefix ("t" . "toggle")
+   :desc "modeline" "M" #'hide-mode-line-mode)
+
+  (:prefix ("w" . "window")
+   :desc "delete others" "1" #'delete-other-windows
+   :desc "delete this" "0" #'delete-window
+   :desc "other window" "o" #'other-window
+   :desc "ace window" "w" #'ace-window
+   :desc "maximize frame" "M" #'toggle-frame-maximized)
+
+  (:prefix ("<tab>" . "workspaces")
+   :desc "switch" "<tab>" #'tab-switch
+   :desc "new" "n" #'tab-new
+   :desc "buffer" "b" #'switch-to-buffer-other-tab
+   :desc "dired" "d" #'dired-other-tab
+   :desc "find file" "f" #'find-file-other-tab
+   :desc "close" "c" #'tab-close
+   :desc "rename" "r" #'tab-rename
+   :desc "undo" "u" #'tab-undo)
+
+  (:prefix ("TAB" . "workspaces")
+   :desc "switch" "TAB" #'tab-switch
+   :desc "new" "n" #'tab-new
+   :desc "buffer" "b" #'switch-to-buffer-other-tab
+   :desc "dired" "d" #'dired-other-tab
+   :desc "find file" "f" #'find-file-other-tab
+   :desc "close" "c" #'tab-close
+   :desc "rename" "r" #'tab-rename
+   :desc "undo" "u" #'tab-undo)))
+
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
+;; unbind `<leader> n' prefix
+(map! :leader :gnvi "n" nil)
+
+(use-package! denote
+  :hook
+  ((text-mode . denote-fontify-links-mode-maybe)
+   (dired-mode . denote-dired-mode)
+   (markdown-mode . denote-dired-mode))
+  :config
+  (setq denote-infer-keywords t)
+  (setq denote-sort-keywords t)
+  (setq denote-prompts '(title keywords))
+  (setq denote-file-type 'org)
+  (setq denote-known-keywords '("emacs" "programming" "algorithm"
+                                "datastructure" "cryptography" "logbook"
+                                "film" "book" "meta" "exams"))
+  :init
+  (map! :leader
+        (:prefix ("n" . "notes")
+         :desc "create" "n" #'denote
+         :desc "dired" "d" #'list-denote
+         :desc "rename" "r" #'denote-rename-file
+         :desc "insert" "i" #'denote-link-or-create
+         :desc "link" "l" #'denote-link-or-create
+         :desc "select extension" "t" #'denote-type
+         :desc "backlink" "b" #'denote-backlink
+
+         (:prefix ("s" . "sequences")
+          :desc "sequence" "n" #'denote-sequence
+          :desc "dired" "f" #'denote-sequence-dired
+          :desc "link" "i" #'denote-sequence-link
+          :desc "link" "l" #'denote-sequence-link
+          :desc "new child" "c" #'denote-sequence-new-child-of-current
+          :desc "reparent" "r" #'denote-sequence-reparent
+          :desc "new sibgling" "s" #'denote-sequence-new-sibling-of-current))))
+
+(use-package! consult-denote
+  :config
+  (consult-denote-mode 1)
+  :init
+  (map! :leader
+        (:prefix ("n" . "notes")
+         :desc "consult find" "f" #'consult-denote-find
+         :desc "consult grep" "g" #'consult-denote-grep)))
+
+(evil-define-key 'normal 'global
+  (kbd "M-a")   #'evil-multiedit-match-symbol-and-next
+  (kbd "M-S-A")   #'evil-multiedit-match-symbol-and-prev)
+
+(evil-define-key 'visual 'global
+  (kbd "M-a")   #'evil-multiedit-match-and-next
+  (kbd "M-S-A")   #'evil-multiedit-match-and-prev)
+
+(evil-define-key '(visual normal) 'global
+  (kbd "C-M-a") #'evil-multiedit-restore)
+
+(with-eval-after-load 'evil-multiedit
+  (evil-define-key 'multiedit 'global
+    (kbd "M-a")   #'evil-multiedit-match-and-next
+    (kbd "M-S-a") #'evil-multiedit-match-and-prev
+    (kbd "RET")   #'evil-multiedit-toggle-or-restrict-region)
+  (evil-define-key '(multiedit multiedit-insert) 'global
+    (kbd "C-n")   #'evil-multiedit-next
+    (kbd "C-p")   #'evil-multiedit-prev))
+
+(setq! +evil-want-o/O-to-continue-comments nil)
+(setq! evil-disable-insert-state-bindings t)
---
+++ packages.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! tron-legacy-theme)
+(package! spacemacs-theme)
+(package! gruber-darker-theme)
+(package! anti-zenburn-theme)
+(package! ef-themes)
+(package! borland-blue-theme)
+
+(package! dired-hide-dotfiles)
+(package! google-translate)
+(package! denote)
+(package! consult-denote)
+
+(package! odin-mode
+  :recipe (:host sourcehut
+           :repo "mgmarlow/odin-mode"))
